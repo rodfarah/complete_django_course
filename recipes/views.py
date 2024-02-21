80c308fa78b992f6c787ea18764deb01cfba63ed
Rodrigo Farah <digofarah@gmail.com>
2024-02-21 17:15:04 -0300

    Start html script. I will study a little bit more of extra content at the end of the course

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(
-        request=request, template_name="recipes/home.html",
+        request=request, template_name="recipes/pages/home.html",
         context={"name": "Rodrigo Farah"})
