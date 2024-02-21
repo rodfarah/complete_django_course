1aa7d78014a57fe96a75b37543860f9acda08979
Rodrigo Farah <digofarah@gmail.com>
2024-02-21 15:59:33 -0300

    Delete example code of basic theory. Restore things in order to start our website

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(
         request=request, template_name="recipes/home.html",
         context={"name": "Rodrigo Farah"})
-
-
-def contact(request):
-    return render(request=request, template_name="recipes/contact.html")
-
-
-def about(request):
-    return HttpResponse("ABOUT")
