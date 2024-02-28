f5c2a7a0b9d64d13342f6c3ca11565ed2546d15c
Rodrigo Farah <digofarah@gmail.com>
2024-02-28 09:58:31 -0300

    Excecute Faker in order to create some data. Templates are not hard coded anymore. Some python inside Jinja on templates.

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(
         request=request, template_name="recipes/pages/home.html",
-        context={"name": "Rodrigo Farah"})
+        context={"recipes": [make_recipe() for n in range(10)]})
 
 
 def recipe(request, id):
     return render(
         request=request, template_name="recipes/pages/recipe-view.html",
-        context={"name": "Rodrigo Farah"})
+        context={"receita": make_recipe()})
---
+++ utils/recipes/factory.py
@@ -0,0 +1,40 @@
+from random import randint
+from faker import Faker
+
+
+def rand_ratio():
+    """Return random width and height numbers"""
+    return randint(840, 900), randint(473, 573)
+
+
+fake = Faker('pt_BR')
+# print(signature(fake.random_number))
+
+
+def make_recipe():
+    w_h = rand_ratio()
+    return {
+        'title': fake.sentence(nb_words=6),
+        'description': fake.sentence(nb_words=12),
+        'preparation_time': fake.random_number(digits=2, fix_len=True),
+        'preparation_time_unit': 'Minutos',
+        'servings': fake.random_number(digits=2, fix_len=True),
+        'servings_unit': 'Porção',
+        'preparation_steps': fake.text(3000),
+        'created_at': fake.date_time(),
+        'author': {
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+        },
+        'category': {
+            'name': fake.word()
+        },
+        'cover': {
+            'url': f'https://loremflickr.com/{w_h[0]}/{w_h[1]}/food,cook',
+        }
+    }
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(make_recipe())
