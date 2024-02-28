d60d5999ce194aa0aea6c7a5667cb4309e95c395
Rodrigo Farah <digofarah@gmail.com>
2024-02-28 11:22:45 -0300

    Footer not aplicable in 'one recipe' template. Recipe description has been aplied instead

--- recipes/views.py
+++ recipes/views.py
@@ -11,4 +11,7 @@ def home(request):
 def recipe(request, id):
     return render(
         request=request, template_name="recipes/pages/recipe-view.html",
-        context={"recipe": make_recipe()})
+        context={
+            "recipe": make_recipe(),
+            "is_detail_page": True
+        })
