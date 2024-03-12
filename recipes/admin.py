cba0c78a4febd7d3158b80a7c31b91b5665c0816
Rodrigo Farah <digofarah@gmail.com>
2024-03-12 10:24:30 -0300

    App and tables have been added to admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
