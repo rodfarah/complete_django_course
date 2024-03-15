e270e5db4ff4af685905302e1f912bc5fa98183d
Rodrigo Farah <digofarah@gmail.com>
2024-03-15 11:24:50 -0300

    Category view added

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(
         request=request, template_name="recipes/pages/home.html",
-        context={"recipes": [make_recipe() for n in range(10)]})
+        context={"recipes": recipes})
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        is_published=True, category__id=category_id).order_by('-id')
+    return render(
+        request, 'recipes/pages/category.html',
+        context={"recipes": recipes})
 
 
 def recipe(request, id):
     return render(
-        request=request, template_name="recipes/pages/recipe-view.html",
+        request, "recipes/pages/recipe-view.html",
         context={
             "recipe": make_recipe(),
             "is_detail_page": True
