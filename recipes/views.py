d2728379e685f0707eca7a19e206811d22f84603
Rodrigo Farah <digofarah@gmail.com>
2024-03-18 15:31:37 -0300

    get_list_or_404 applied to category page

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 # from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -11,13 +11,14 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        is_published=True, category__id=category_id).order_by('-id')
+    recipes = get_list_or_404(klass=Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+
     return render(
         request, 'recipes/pages/category.html',
         context={
             "recipes": recipes,
-            "category_name": f'{recipes.first().category.name} - Category |'
+            "category_name": f'{recipes[0].category.name} - Category |'
         }
     )
 
