45f6147cc5b123fcf7282cfd6abb130518cf93d4
Rodrigo Farah <digofarah@gmail.com>
2024-03-22 14:34:39 -0300

    View Functions test is now implemented

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from . import views
 
 
 class RecipeURLsTest(TestCase):
@@ -14,3 +15,17 @@ class RecipeURLsTest(TestCase):
     def test_recipe_onerecipe_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'one_id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_onerecipe_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'one_id': 1}))
+        self.assertIs(view.func, views.recipe)
