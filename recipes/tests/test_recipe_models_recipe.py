3c441a05ae52f078d91532e7eee19ddeaa0cf06d
Rodrigo Farah <digofarah@gmail.com>
2024-04-05 17:32:01 -0300

    test_recipe_preparation_steps_is_html_is_false_by_default now implemented

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -1,6 +1,7 @@
 from .test_recipe_base import RecipeTestBase
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
+from recipes.models import Recipe
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -18,3 +19,22 @@ class RecipeModelTest(RecipeTestBase):
         setattr(self.recipe, field_name, "A" * (max_lenght + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='John Doe'),
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutes',
+            servings=5,
+            servings_unit='Portions',
+            preparation_steps='Recipe Preparation Steps',
+        )
+        # recipe.full_clean() validates all conditions created on Models
+        recipe.full_clean()
+        # recipe.save() saves the code above on database
+        recipe.save()
+        self.assertFalse(recipe.preparation_steps_is_html)
