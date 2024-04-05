476914be7e15768527e6c74571f133c31a51858c
Rodrigo Farah <digofarah@gmail.com>
2024-04-05 14:27:59 -0300

    Lots of tests now implemented

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -38,6 +38,11 @@ class RecipeViewsTest(RecipeTestBase):
         # check how many recipes were added
         self.assertEqual(len(context), 1)
 
+    def test_recipe_home_template_doesnt_load_not_published_recipes(self):
+        self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:home'))
+        self.assertFalse(len(response.content) == 0)
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
@@ -49,6 +54,12 @@ class RecipeViewsTest(RecipeTestBase):
                 kwargs={'category_id': 1000}))
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_doesnt_load_not_published_recipes(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+        self.assertFalse(len(response.content) == 0)
+
     def test_recipe_onerecipe_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -59,3 +70,38 @@ class RecipeViewsTest(RecipeTestBase):
                 'recipes:recipe',
                 kwargs={'id': 1000}))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        self.make_recipe(title=needed_title)
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        # content is the template html code. Check it out by using test debug with breakpoint.
+        content = response.content.decode('utf-8')
+        # response.context['recipes'] is a queryset . Check it out by using test debug with breakpoint.
+        self.assertIn(needed_title, content)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_correct_recipe(self):
+        needed_title = 'This is a detail page - It loads one recipe'
+        self.make_recipe(title=needed_title)
+        response = self.client.get(reverse(
+            'recipes:recipe',
+            kwargs={'id': 1}
+        ))
+        # content is the template html code. Check it out by using test debug with breakpoint.
+        content = response.content.decode('utf-8')
+        self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_doesnt_load_not_published_recipes(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id}))
+        self.assertFalse(len(response.content) == 0)
