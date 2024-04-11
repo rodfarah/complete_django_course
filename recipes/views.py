68a2943c08042453c167eaf1bf9104c9a44d223b
Rodrigo Farah <digofarah@gmail.com>
2024-04-11 16:13:49 -0300

    continuing to implement search view function

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 # from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
@@ -34,4 +35,9 @@ def recipe(request, id):
 
 
 def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
     return render(request, 'recipes/pages/search.html')
