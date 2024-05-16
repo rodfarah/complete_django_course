391a8603324fcdce15f98ac38b7065fdfe4d470a
Rodrigo Farah <digofarah@gmail.com>
2024-05-16 10:34:42 -0300

    starts to set backend logic to register form in order to forbid user to reload form page with filled forms

--- authors/views.py
+++ authors/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import Http404
 
 
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {'form': form})
 
 
@@ -15,5 +13,7 @@ def register_create(request):
     if not request.POST:
         raise Http404
     else:
-        form = RegisterForm(request.POST)
-    return render(request, 'authors/pages/register_view.html', {'form': form})
+        POST = request.POST
+        request.session['register_form_data'] = POST
+        form = RegisterForm(POST)
+    return redirect(to='authors:register')
