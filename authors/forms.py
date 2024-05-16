94b7255e45f1dd9622c9551885405b3c2d9b777a
Rodrigo Farah <digofarah@gmail.com>
2024-05-16 11:11:25 -0300

    formats labels, help texts, error messages and placeholders for form fields

--- authors/forms.py
+++ authors/forms.py
@@ -6,3 +6,33 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+        # exclude = ['first_name]
+
+        labels = {
+            'username': 'Username',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'The e-mail address must be valid'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username here',
+                # sobrescreve ou adiciona classes html!
+                'class': 'input text-input other-class',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
