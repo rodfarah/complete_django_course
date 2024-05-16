7396dfffa96cded9a25a1de0d71e853af71cbb72
Rodrigo Farah <digofarah@gmail.com>
2024-05-16 13:45:43 -0300

    Improves RegisterForms with __init__ and widgets personalizations

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,33 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'ex. fulano@fluano.com')
+        add_placeholder(self.fields['first_name'], 'Your first name')
+
+        add_attr(self.fields['last_name'], 'placeholder', 'Your last name')
+
+    password2 = forms.CharField(
+        required=True,
+
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Type password again'
+        })
+    )
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
