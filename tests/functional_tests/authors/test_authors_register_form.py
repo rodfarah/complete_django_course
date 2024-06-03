1a07e26d71c42c26352a7ad896721db7572c58b2
Rodrigo Farah <digofarah@gmail.com>
2024-06-03 16:42:56 -0300

    Tests if empty first name raises an error

--- tests/functional_tests/authors/test_authors_register_form.py
+++ tests/functional_tests/authors/test_authors_register_form.py
@@ -1,7 +1,38 @@
 from .base import AuthorsBaseTest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class AuthorsRegisterTest(AuthorsBaseTest):
-    def test_the_test(self):
+
+    def get_by_placeholder(self, web_element: str, placeholder_text: str):
+        field = web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder_text}"]')
+        return field
+
+    def fill_form_dummy_data(self, form):
+        fields = form.find_elements(By.TAG_NAME, "input")
+        for field in fields:
+            if field.is_displayed():
+                field.send_keys(' ' * 20)
+
+    def test_empty_first_name_error_message(self):
         self.browser.get(self.live_server_url + "/authors/register/")
-        self.sleep()
+        form = self.browser.find_element(
+            By.XPATH, '/html/body/main/div[2]/form'
+        )
+        self.fill_form_dummy_data(form=form)
+        form.find_element(
+            By.NAME,
+            'email'
+        ).send_keys('dummy@email.com')
+        first_name_field = self.get_by_placeholder(form, "First Name")
+        first_name_field.send_keys(' ')
+        first_name_field.send_keys(Keys.ENTER)
+
+        form = self.browser.find_element(
+            By.XPATH, '/html/body/main/div[2]/form'
+        )
+
+        self.sleep(3)
+        self.assertIn("Write your first name", form.text)
