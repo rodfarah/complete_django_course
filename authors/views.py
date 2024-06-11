a6cdc72c87a10ec39f71cde6ef5eebbfbae07317
Rodrigo Farah <digofarah@gmail.com>
2024-06-11 15:11:03 -0300

    starts to set user dashboard data

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from recipes.models import Recipe
+
 from .login_register_forms import LoginForm, RegisterForm
 
 
@@ -83,4 +85,12 @@ def logout_view(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard_view(request):
-    return render(request, 'authors/pages/dashboard.html')
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+    )
+    return render(request, 'authors/pages/dashboard.html',
+                  context={
+                      'recipes': recipes
+                  }
+                  )
