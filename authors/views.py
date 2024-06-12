e2118f734598e1c9d844677512c6b674bdda8530
Rodrigo Farah <digofarah@gmail.com>
2024-06-12 11:10:56 -0300

    user may edit his own recipes

--- authors/views.py
+++ authors/views.py
@@ -110,11 +110,19 @@ def dashboard_recipe_edit(request, id):
 
     form = AuthorRecipeForm(
         data=request.POST or None,
+        files=request.FILES or None,
         instance=recipe
     )
 
     if form.is_valid():
-        form.save()
+        form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        recipe.save()
+
+        messages.success(request, 'Your recipe has been successfully saved.')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
 
     return render(
         request, 'authors/pages/dashboard_recipe.html',
